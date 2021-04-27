324d432e30e698f649237dc4143d2cb16c6d18e5
Pedro Pontes <pontescpedro@gmail.com>
2021-04-26 21:24:52 -0300

    Refactor code and add scrape more info for ethereum

--- utils/cryptocurrency/ethereum.js
+++ utils/cryptocurrency/ethereum.js
@@ -2,22 +2,30 @@ const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
 const webpageUrl = 'https://www.infomoney.com.br/cotacoes/ethereum-eth/';
-const selector = 'div[class="value"] p';
 
-const getValue = async () => {
+const selectors = {
+  value: 'div[class="value"] p',
+  valueCurrency: 'div[class="value"] label',
+  dayChange: 'div[class="percentage"] p',
+  dayMin: 'div[class="minimo"] p',
+  dayMax: 'div[class="maximo"] p',
+};
+
+const getInfo = async () => {
   const response = await fetch(webpageUrl);
   const htmlText = await response.text();
 
   const cheerioProcess = cheerio.load(htmlText);
 
-  const value = cheerioProcess(selector).text();
+  const resultInfo = {};
 
-  console.log(value);
+  for (attribute of Object.keys(selectors)) {
+    resultInfo[attribute] = cheerioProcess(selectors[attribute]).text();
+  }
 
-  return value;
+  return resultInfo;
 };
 
 module.exports = {
-  webpageUrl,
-  getValue,
+  getInfo,
 };
